5de30768c2a00b580f34c6148c20352098214651
shalev-habany <shalev.habany@gmail.com>
2022-11-17 15:03:44 +0200

    second commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Layout from './Components/LayoutArea/Layout/Layout';
+import { BrowserRouter } from 'react-router-dom';
+import interceptorsService from './Services/InterceptorsService';
+import { Provider } from 'react-redux';
+import { productsStore } from './Redux/ProductsState';
+
+interceptorsService.createInterceptors();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+        <Provider store={productsStore}>
+          <Layout />
+        </Provider>
+    </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
